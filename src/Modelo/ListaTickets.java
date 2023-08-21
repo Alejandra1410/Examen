695c54f3827ce8c65f0311ffdc38ffd50ab5fefd
abiga <abigailmolina201@gmail.com>
2023-08-21 14:22:30 -0600

    ListaTickets
    Se creo la clase Lista Tickets

--- src/Modelo/ListaTickets.java
+++ src/Modelo/ListaTickets.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Modelo;
 
 /**
@@ -9,5 +5,13 @@ package Modelo;
  * @author abiga
  */
 public class ListaTickets {
-    
+   public void eliminarticket(String numero){
+       
+   }
+   public void agregarticket(){
+       
+   }
+   public void Buscarticket(String tipo){
+       
+   }
 }
