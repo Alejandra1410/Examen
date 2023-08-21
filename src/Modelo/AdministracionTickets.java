8794bd0e77ed49fcd724a5bf0fcb1be528f70c76
abiga <abigailmolina201@gmail.com>
2023-08-21 14:19:10 -0600

    AdministracionTickets
    Se creo la clase Administracion con sus constructores, get and setters

--- src/Modelo/AdministracionTickets.java
+++ src/Modelo/AdministracionTickets.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Modelo;
 
 /**
@@ -9,5 +5,39 @@ package Modelo;
  * @author abiga
  */
 public class AdministracionTickets {
+    private String tickets;
+    private String numero;
+    private String tipo;
+
+    public AdministracionTickets(String tickets, String numero, String tipo) {
+        this.tickets = tickets;
+        this.numero = numero;
+        this.tipo = tipo;  
+    }
+    public String getTickets() {
+        return tickets;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTickets(String tickets) {
+        this.tickets = tickets;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+    
+    
     
 }
